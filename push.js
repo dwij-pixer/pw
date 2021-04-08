72aa879e4e180fdb17d35f3c81f14b74ab3643a2
DwijVirani <viranidwij8@gmail.com>
2021-04-08 14:55:27 +0530

    fix

--- push.js
+++ push.js
@@ -5,12 +5,14 @@ const vapidKeys = {
     privateKey: 'XNjVJ8aIA6Lf4EHg59J2uk_ljx_2r69UOCg0kyDrfoU'    
 } 
 
-push.setVapidDetails('mailto: test@test.com', vapidKeys.privateKey, vapidKeys.publicKey);
+push.setVapidDetails('mailto: test@test.com', vapidKeys.publicKey, vapidKeys.privateKey);
 
-let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/egNNKp5Ny6g:APA91bF6CYQ93SgBYS7Ydo5ltZlWMggj6Ku351dzqCviOM1da3kfsgQicRCISSa3iZBisBRbRzCwRb3dTY7zlJ3SmvCy8mjXxr1Yk6Dd5Dm7_gVMve5giTkr1aEHNwj7HMGUHHFkYET2",
-expirationTime : null,
-keys:{
-    p256dh: "BLuSVUYct39SrvSMRY-NHaXLkdiI2YMPXahmXlhGe48wPp0hzEeQrfGCFeT2RxwigjF7n0i7mCIw0OjLWKpqOTM",
-    auth:"AbzfhyRQADF1-IxN_jzb8A"
-}}
+let sub = {
+    endpoint: "https://fcm.googleapis.com/fcm/send/egNNKp5Ny6g:APA91bF6CYQ93SgBYS7Ydo5ltZlWMggj6Ku351dzqCviOM1da3kfsgQicRCISSa3iZBisBRbRzCwRb3dTY7zlJ3SmvCy8mjXxr1Yk6Dd5Dm7_gVMve5giTkr1aEHNwj7HMGUHHFkYET2",
+    expirationTime: null,
+    keys:{
+        p256dh: "BLuSVUYct39SrvSMRY-NHaXLkdiI2YMPXahmXlhGe48wPp0hzEeQrfGCFeT2RxwigjF7n0i7mCIw0OjLWKpqOTM",
+        auth: "AbzfhyRQADF1-IxN_jzb8A"
+    }
+}
 push.sendNotification(sub, 'text message');
